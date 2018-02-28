c8f3e314f19dbe8cf7d4df12e3683177be160ff4
chesthighwalls <seanreeves@gmail.com>
2018-02-27 20:18:28 -0500

    Added bash script and service file.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Author: Sean Reeves
+# Date Created: 02/27/2018
+# Date Modified: 
+# Description: 
+# License: MIT License
+
+if ! [ $(id -u) = 0 ]; then
+   echo "script must be run as root"
+   exit 1
+fi
+
+sudo mv my_assistant.service /lib/systemd/system/ 
+sudo systemctl enable my_assistant.service
+sudo service my_assistant start
+sudo systemctl my_assistant.service status
+

576382bac7c9230ceed931767907927d956db89e
chesthighwalls <seanreeves@gmail.com>
2018-03-31 20:41:25 -0400

    Changed 'mv' to 'cp'

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 
-sudo mv my_assistant.service /lib/systemd/system/ 
+sudo cp my_assistant.service /lib/systemd/system/ 
 sudo systemctl enable my_assistant.service
 sudo service my_assistant start
 sudo systemctl my_assistant.service status
